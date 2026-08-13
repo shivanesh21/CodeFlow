20ce6d2b178f0762d090a92cb4de78882f7c161b
Shivanesh B <shivaneshbalasubramaniam@gmail.com>
2026-08-13 11:46:42 +0530

    fix: load dotenv in api handler and express app for vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import app from "../server/app.js";
 import connectDB from "../server/config/db.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../server/.env") });
+dotenv.config();
 
 export default async function handler(req, res) {
   try {
--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 import authRoutes from "./routes/authRoutes.js";
 import codeSnippetRoutes from "./routes/CodeSnippetRoutes.js";
